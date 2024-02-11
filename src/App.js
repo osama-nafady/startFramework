3d9297de3b29f006f8eeba2b668cd7dddac5e1df
Osama Nafady <dev@nes.moe.edu.eg>
2024-02-11 02:00:22 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+import Layout from './components/About/About';
 
 let routers = createBrowserRouter([
   {
 path:'', element: <Layout/>, children: [
-  {}
+  {index:true, element: <Home/>},
+  {path:'about', element: <About/>},
 ]
 }]);
 
